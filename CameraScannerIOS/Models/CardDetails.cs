e129a04824498c0e4cf2bccb5a545e29d58151a9
Kristian <xanril@users.noreply.github.com>
2024-09-10 18:59:59 +0800

    add model for card details and scanned item

---
+++ CameraScannerIOS/Models/CardDetails.cs
@@ -0,0 +1,7 @@
+namespace CameraScannerIOS.Models;
+
+public class CardDetails
+{
+    public string Name { get; set; }
+    public string CreditCardNumber { get; set; }
+}
---
+++ CameraScannerIOS/Models/ScannedItem.cs
@@ -0,0 +1,7 @@
+namespace CameraScannerIOS.Models;
+
+public class ScannedItem
+{
+    public string Text { get; set; }
+    public CGRect Bounds { get; set; }
+}
